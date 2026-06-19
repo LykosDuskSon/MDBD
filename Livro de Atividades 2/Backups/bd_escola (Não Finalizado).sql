dc92f36b537b478281607fe252d5b8149d7e693b
Nicolas_Maia <nigmaia2009@gmail.com>
2026-06-19 11:49:33 -0300

    Add files via upload

---
+++ Livro de Atividades 2/Backups/bd_escola (Não Finalizado).sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19/06/2026 às 14:44
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bd_escola`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `idAlunos` int(11) NOT NULL,
+  `Nome` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `contatos`
+--
+
+CREATE TABLE `contatos` (
+  `idContatos` int(11) NOT NULL,
+  `Telefone` char(9) NOT NULL,
+  `Email` varchar(320) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `dados_pessoais`
+--
+
+CREATE TABLE `dados_pessoais` (
+  `idDados` int(11) NOT NULL,
+  `CPF` char(11) NOT NULL,
+  `Data_Nascimento` date NOT NULL,
+  `Idade` int(2) NOT NULL,
+  `Sexo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `responsaveis`
+--
+
+CREATE TABLE `responsaveis` (
+  `idResponsavel` int(10) NOT NULL,
+  `Nome` varchar(50) NOT NULL,
+  `Parentesco` varchar(15) NOT NULL,
+  `idDados` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `responsaveisalunos`
+--
+
+CREATE TABLE `responsaveisalunos` (
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `alunos`
+--
+ALTER TABLE `alunos`
+  ADD PRIMARY KEY (`idAlunos`);
+
+--
+-- Índices de tabela `contatos`
+--
+ALTER TABLE `contatos`
+  ADD PRIMARY KEY (`idContatos`);
+
+--
+-- Índices de tabela `dados_pessoais`
+--
+ALTER TABLE `dados_pessoais`
+  ADD PRIMARY KEY (`idDados`);
+
+--
+-- Índices de tabela `responsaveis`
+--
+ALTER TABLE `responsaveis`
+  ADD PRIMARY KEY (`idResponsavel`),
+  ADD KEY `fk_responsaveis_dados` (`idDados`);
+
+--
+-- Índices de tabela `responsaveisalunos`
+--
+ALTER TABLE `responsaveisalunos`
+  ADD KEY `idAluno` (`idAluno`),
+  ADD KEY `idResponsavel` (`idResponsavel`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `alunos`
+--
+ALTER TABLE `alunos`
+  MODIFY `idAlunos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `contatos`
+--
+ALTER TABLE `contatos`
+  MODIFY `idContatos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `dados_pessoais`
+--
+ALTER TABLE `dados_pessoais`
+  MODIFY `idDados` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `responsaveis`
+--
+ALTER TABLE `responsaveis`
+  MODIFY `idResponsavel` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+ 
+--
+-- Restrições para tabelas `responsaveis`
+--
+ALTER TABLE `responsaveis`
+  ADD CONSTRAINT `fk_responsaveis_dados` FOREIGN KEY (`idDados`) REFERENCES `dados_pessoais` (`idDados`),
+  ADD CONSTRAINT `responsaveis_ibfk_1` FOREIGN KEY (`idDados`) REFERENCES `dados_pessoais` (`idDados`);
+
+--
+-- Restrições para tabelas `responsaveisalunos`
+--
+ALTER TABLE `responsaveisalunos`
+  ADD CONSTRAINT `responsaveisalunos_ibfk_1` FOREIGN KEY (`idAluno`) REFERENCES `alunos` (`idAlunos`),
+  ADD CONSTRAINT `responsaveisalunos_ibfk_2` FOREIGN KEY (`idResponsavel`) REFERENCES `responsaveis` (`idResponsavel`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
